d4666fe5f2df427bf5d0222a19b01e991c753c35
Lucas Silva <lucascarlosjobs@gmail.com>
2024-03-31 17:01:36 -0300

    rebase main and master

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from "primevue/button"
+</script>
+
+<template>
+  <p>Hello world!</p>
+  <Button label="Submit" icon="pi pi-check" />
+</template>
+
+<style scoped>
+</style>
